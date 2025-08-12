0103ede2677622db22dbdce3118f1de633ad35bf
harukamiki <nahahamachine1@gmail.com>
2025-08-12 23:51:47 +0900

    マイグレーションなどの作成

--- src/database/migrations/2025_08_12_224110_create_addresses_table.php
+++ src/database/migrations/2025_08_12_224110_create_addresses_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->string('img',255)->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('post_code',255);
             $table->string('address',255);
             $table->timestamps();
@@ -30,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('addresses');
     }
 };
